d2aa19e9a00714bafa63e96c566819c010ddd0d1
Sejo <sejo1981@gmail.com>
2019-09-10 13:08:38 +0300

    Updates

---
+++ precache-manifest.89747fd7d2477548f1934aaf3b0045ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8e55390c9ab92f551fd4ab7a967739",
+    "url": "/Sergey-Bekker-04-09-2019/index.html"
+  },
+  {
+    "revision": "7011ad09834e5dae47be",
+    "url": "/Sergey-Bekker-04-09-2019/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d1d289f927df6dcd13d1",
+    "url": "/Sergey-Bekker-04-09-2019/static/js/2.60c69b3c.chunk.js"
+  },
+  {
+    "revision": "7011ad09834e5dae47be",
+    "url": "/Sergey-Bekker-04-09-2019/static/js/main.ea09bf18.chunk.js"
+  },
+  {
+    "revision": "8c694764dd40004a112a",
+    "url": "/Sergey-Bekker-04-09-2019/static/js/runtime~main.d47ad2b4.js"
+  }
+]);
